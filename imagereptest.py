dfff3594c22569f2242483be78c5467a1ec52684
swagking22 <st018044@schoolemail.net>
2024-06-11 15:59:47 +0100

    added the image representation test
    includes the image representation test code which loads the test photo in the middle of the screen using rez of the screen + rez of the test pic plus the test pic itself

---
+++ imagereptest.py
@@ -0,0 +1,28 @@
+import pygame
+
+# pygame setup
+pygame.init()
+width = 1280; vert = 720
+screen = pygame.display.set_mode((width, vert))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+
+    jonk = pygame.image.load("jonktest.png")
+    jonkrez = [184,346]
+    screen.blit(jonk, (width/2-jonkrez[0]/2,vert/2-jonkrez[1]/2))
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
