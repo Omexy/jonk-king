b8615fc1012592a6702e908d8c5710200176da91
Daniel <44512200+Omexy@users.noreply.github.com>
2024-06-13 09:37:20 +0100

    Screen size based on monitor resolution

--- jonk-king.py
+++ jonk-king.py
@@ -1,11 +1,12 @@
-import pygame
+import pygame, sys, ctypes
+user32 = ctypes.windll.user32
+monitorRes = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
 pygame.display.set_caption('Jonk King')
 icon = pygame.image.load('jonk-king.ico')
 pygame.display.set_icon(icon)
 pygame.init()
-width = 1280; vert = 720
-screen = pygame.display.set_mode((width, vert))
+screen = pygame.display.set_mode(monitorRes)
 clock = pygame.time.Clock()
 running = True
 
@@ -13,6 +14,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
 pygame.display.flip()
 clock.tick(60)
