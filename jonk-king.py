063f3650efbe79a6ae65afaf214dd4e5c8ad1eb2
Daniel <44512200+Omexy@users.noreply.github.com>
2024-06-13 10:05:00 +0100

    Comments & variable name changes

--- jonk-king.py
+++ jonk-king.py
@@ -1,23 +1,22 @@
-import pygame, sys, ctypes, time
-user32 = ctypes.windll.user32
-monitorRes = [1280, 720]
+import pygame, sys, ctypes, time #Relevant modules imported
+resolution = [1280, 720]
 
-pygame.display.set_caption('Jonk King')
-icon = pygame.image.load('jonk-king.ico')
-pygame.display.set_icon(icon)
+pygame.display.set_caption('Jonk King') #Window title
+icon = pygame.image.load('jonk-king.ico') #Loads icon into pygame
+pygame.display.set_icon(icon) #Window icon
 pygame.init()
-screen = pygame.display.set_mode(monitorRes)
+screen = pygame.display.set_mode(resolution) #Sets resolution to 1280x720
 clock = pygame.time.Clock()
 running = True
 
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: #Quits game when X is clicked
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+            if event.key == pygame.K_ESCAPE: #Quits game when ESC is pressed
                 running = False
 
-pygame.display.flip()
-clock.tick(60)
+pygame.display.flip() #Allows for images to be added
+clock.tick(60) #Sets FPS to 60
 pygame.quit()
